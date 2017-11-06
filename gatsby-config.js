db0f715302a9fa590bab6bdc7b0e02f7dfe7efb1
Alan Shaw <alan@tableflip.io>
2017-11-06 16:52:38 +0000

    uses gatsby-source-tune module

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'facts',
-        path: `${__dirname}/facts.json`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: `${__dirname}/pages/`
-      }
-    },
-    'gatsby-transformer-json'
+    'gatsby-source-tune'
   ]
 }
