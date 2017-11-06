6c18c1eb332a1420e1714bdd26f9e5d28a5514fc
Oli Evans <oli@zilla.org.uk>
2017-11-06 16:25:59 +0000

    Adds helpline box

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,16 @@ import Helmet from 'react-helmet'
 import 'tachyons'
 import logoSrc from './logo.png'
 
+const HelplineBox = () => (
+  <div className='pa2 dib tl' style={{border: '4px solid #800080', color: '#800080'}}>
+    <img src='https://icon.now.sh/phone_iphone/800080' className='dib v-mid' style={{height: 42, width: 42}} />
+    <div className='dib v-mid f4 fw7'>
+      <div className='tracked'>HELPLINE</div>
+      <div>0845 451 2547</div>
+    </div>
+  </div>
+)
+
 const Infobar = () => (
   <div className='pv3 tc' style={{background: '#ffda31'}}>
     <span className=''>
@@ -17,10 +27,13 @@ const Infobar = () => (
 )
 
 const Header = () => (
-  <header className='db bg-white pa4 mw8 center tc tl-ns' >
-    <Link to='/' style={{textDecoration: 'none', display: 'inline-block'}}>
-      <img src={logoSrc} alt={'Aanchal Woman\'s Aid'} style={{width: 196, height: 54}} />
+  <header className='db dt-ns w-100 bg-white pa4 mw8 center tc tl-ns' >
+    <Link to='/' className='db dtc-ns no-underline' >
+      <img src={logoSrc} alt={'Aanchal Woman\'s Aid'} style={{height: 70}} />
     </Link>
+    <div className='db dtc-ns v-top tc tr-ns mt4 mt0-ns'>
+      <HelplineBox />
+    </div>
   </header>
 )
 
