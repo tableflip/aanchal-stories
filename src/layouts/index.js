34ad85660c6c1b245112f6c3dc4aa278fc900250
Oli Evans <oli@zilla.org.uk>
2017-11-07 09:56:21 +0000

    Fettling and quote

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,8 +7,8 @@ import 'tachyons'
 import logoSrc from './logo.png'
 
 const HelplineBox = () => (
-  <div className='pa2 dib tl' style={{border: '4px solid #800080', color: '#800080'}}>
-    <img src='https://icon.now.sh/phone_iphone/800080' className='dib v-mid' style={{height: 42, width: 42}} />
+  <div className='pa2 dib tl purple b-purple' style={{border: '4px solid'}}>
+    <img src='https://icon.now.sh/phone_iphone/5e2ca5' className='dib v-mid' style={{height: 42, width: 42}} />
     <div className='dib v-mid f4 fw7'>
       <div className='tracked'>HELPLINE</div>
       <div>0845 451 2547</div>
@@ -17,7 +17,7 @@ const HelplineBox = () => (
 )
 
 const Infobar = () => (
-  <div className='pv3 tc' style={{background: '#ffda31'}}>
+  <div className='pv3 tc bg-yellow'>
     <span className=''>
       <span className='mr4'>Call us any time</span>
       <img style={{verticalAlign: '-2px'}} src='https://icon.now.sh/phone' />
