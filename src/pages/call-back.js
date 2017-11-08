d93875f9d9e4d5ed472806f248868e93ef894467
Oli Evans <oli@zilla.org.uk>
2017-11-08 13:17:05 +0000

    Adds call-back-ok page

--- src/pages/call-back.js
+++ src/pages/call-back.js
@@ -2,6 +2,24 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+const CallBackForm = () => (
+  <form className='pa3 black-80' method='post' action='https://post.tableflip.io/test.aanchal.org.uk'>
+    <div className='measure-narrow'>
+      <label for='phone-number' className='f6 b db mb2'>Your phone number</label>
+      <input autoFocus className='input-reset ba b--black-20 pa3 mb2 db w-100 f4 courier' type='tel' id='phone-number' name='phone-number' aria-describedby='phone-number-desc' />
+      <small id='phone-number-desc' className='f6 lh-copy black-60 db mb2'>
+        e.g. <span className='courier'>08454512547</span>
+      </small>
+      <div className='mt4'>
+        <button to='call-back' className='bn f5 fw4 link br1 ph3 pv3 dib white bg-aanchal-green pointer'>
+          <img className='v-mid mr2' src='https://icon.now.sh/ring_volume/ffffff/22' alt='' />
+          Request a call back
+        </button>
+      </div>
+    </div>
+  </form>
+)
+
 const CallBackPage = ({data}) => (
   <div className='tl pb6'>
     <Helmet title={data.home.pageTitle} />
@@ -11,25 +29,11 @@ const CallBackPage = ({data}) => (
     </p>
 
     <p className='ph3 f4 f4-ns lh-copy center' style={{maxWidth: '560px'}}>
-      Type in your phone number then click send. We'll call you back.
+      Type in your phone number then click the button. We'll call you back.
     </p>
 
     <div className='center tl' style={{maxWidth: '560px'}}>
-      <form className='pa3 black-80'>
-        <div className='measure-narrow'>
-          <label for='phone-number' className='f6 b db mb2'>Your phone number</label>
-          <input autoFocus className='input-reset ba b--black-20 pa3 mb2 db w-100 f4 courier' type='tel' id='phone-number' aria-describedby='phone-number-desc' />
-          <small id='phone-number-desc' className='f6 lh-copy black-60 db mb2'>
-            e.g. <span className='courier'>08454512547</span>
-          </small>
-          <div className='mt4'>
-            <button to='call-back' className='bn f5 fw4 link br1 ph3 pv3 dib white bg-aanchal-green pointer'>
-              <img className='v-mid mr2' src='https://icon.now.sh/ring_volume/ffffff/22' alt='' />
-              Request call back
-            </button>
-          </div>
-        </div>
-      </form>
+      <CallBackForm />
     </div>
   </div>
 )
