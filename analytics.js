a907363254b34f0defc3ecc34376a09ab01464ef
TABS <tabs@tableflip.io>
2018-08-07 13:28:00 +0100

    Built by TABS

--- analytics.js
+++ analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || []
 function gtag () { window.dataLayer.push(arguments) }
 gtag('js', new Date())
-gtag('config', 'UA-52178211-4')
+gtag('config', 'UA-123396830-1')
